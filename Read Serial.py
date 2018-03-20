13d6470db48daf1dc58b71f3bb3fa0fab18afb65
Jucin102 <35310010+Jucin102@users.noreply.github.com>
2018-03-19 23:04:57 -0400

    Add files via upload

---
+++ Read Serial.py
@@ -0,0 +1,22 @@
+import serial
+import time
+from Tkinter import*
+
+baudRate=9600
+comport='com3' #add user input
+
+data=serial.Serial(comport,baudRate)
+
+root = Tk()
+canvas = Canvas(root, width=640, height=480, bg="white")
+canvas.pack()
+
+id = canvas.create_text(320,240, text='CONNECTION ESTABLISHED: '+ comport + ', ' + str(baudRate))
+canvas.update()
+time.sleep(2)
+x=1
+while x<2:
+   data_raw = data.readline()
+   canvas.itemconfigure(id, text=data_raw)
+   root.update()
+root.mainloop()
