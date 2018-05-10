f41dabc745d1503bc01dbb6fd7c0360cfe11dfa7
derpmastear <kevinypw@gmail.com>
2018-05-10 12:41:26 -0400

    Added the COMM stuff
    The Communications files went in the ROV GUI folder, because I couldn't download the entire project (lag issues)
    
    I took all of the stuff from MainPage.xaml.cs and combined it with my code.

--- ROV GUI/MainPage.xaml.cs
+++ ROV GUI/MainPage.xaml.cs
@@ -1,78 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Windows.Gaming.Input;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Devices.SerialCommunication;
 using Windows.Devices.Enumeration;
 using Windows.Storage.Streams;
 
+
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Hello_World
+namespace ROV_GUI
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private String myKey;
-        private RawGameController pilotOneControl;
-        private RawGameController pilotTwoControl;
-        private Timer pilotTimer;
-        private int buttonCount;
-        private int axisCount;
-        private Boolean[] buttonStates;
-        private Double[] axisStates;
-        private Boolean controllersOnline;
-        private int currentController;
-
         private SerialDevice UartPort;
         private DataReader DataReaderObject = null;
         private DataWriter DataWriterObject;
         private CancellationTokenSource ReadCancellationTokenSource;
+
         public MainPage()
         {
             this.InitializeComponent();
-            myKey = "";
-            AutoResetEvent AutoEvent = new AutoResetEvent(true);
-            pilotTimer = new Timer(new TimerCallback(checkStatus), AutoEvent, 0, 1);
-            currentController = 0;
-
             string selector = SerialDevice.GetDeviceSelector("COM3");
         }
-        private void HorizontalButton_Click(object sender, RoutedEventArgs e)
+
+        private void Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
 
         }
-        private void HippityHoppityYoureMyProperty(object sender, RoutedEventArgs e)
-        {
-            PilotOne.Text = myKey;
-        }
-        void MainPage_KeyDown(object sender, KeyRoutedEventArgs e)
-        {
-            myKey = e.Key + "";
-            HippityHoppityYoureMyProperty(this, new RoutedEventArgs());
-        }
-        private void checkStatus(object state)
-        {
-            if(controllersOnline)
-            {
 
-            }
-        }
         public async Task Initialise(uint BaudRate)     //NOTE - THIS IS AN ASYNC METHOD!
         {
             try
