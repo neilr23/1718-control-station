35e6b5271b92258192e4da7964a4d3f6a124e88b
derpmastear <kevinypw@gmail.com>
2018-05-10 11:13:57 -0400

    controller stuff

--- Hello World/Hello World/Hello World/MainPage.xaml.cs
+++ Hello World/Hello World/Hello World/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Gaming.Input;
 using System.Threading;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -24,24 +25,43 @@ namespace Hello_World
     public sealed partial class MainPage : Page
     {
         private String myKey;
+        private RawGameController pilotOneControl;
+        private RawGameController pilotTwoControl;
+        private Timer pilotTimer;
+        private int buttonCount;
+        private int axisCount;
+        private Boolean[] buttonStates;
+        private Double[] axisStates;
+        private Boolean controllersOnline;
+        private int currentController;
         public MainPage()
         {
             this.InitializeComponent();
             myKey = "";
-        };
+            AutoResetEvent AutoEvent = new AutoResetEvent(true);
+            pilotTimer = new Timer(new TimerCallback(checkStatus), AutoEvent, 0, 1);
+            currentController = 0; 
+        }
 
         private void HorizontalButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        private void TippityTappityYoureMyProperty(object sender, RoutedEventArgs e)
+        private void HippityHoppityYoureMyProperty(object sender, RoutedEventArgs e)
         {
             PilotOne.Text = myKey;
         }
-        void Address_KeyDown(object sender, KeyRoutedEventArgs e)
+        void MainPage_KeyDown(object sender, KeyRoutedEventArgs e)
         {
             myKey = e.Key + "";
-            TippityTappityYoureMyProperty(this, new RoutedEventArgs());
+            HippityHoppityYoureMyProperty(this, new RoutedEventArgs());
+        }
+        private void checkStatus(object state)
+        {
+            if(controllersOnline)
+            {
+
+            }
         }
     }
 }
