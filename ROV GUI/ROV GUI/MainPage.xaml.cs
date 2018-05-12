7c1cf387a62c9f17e04edd01c3d28bd2a3f4b3c4
Patton6 <pjmullin6@comcast.net>
2018-05-12 01:10:36 -0400

    this sh*t works
    lol

--- ROV GUI/ROV GUI/MainPage.xaml.cs
+++ ROV GUI/ROV GUI/MainPage.xaml.cs
@@ -205,132 +205,135 @@ namespace ROV_GUI
         
         async private void checkAndSend(object state)//We send things here
         {
-            string deploy = "";
-            try
+            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
             {
-                if (!(RawGameControllers.Count == 0))
+                string deploy = "";
+                try
                 {
-                    controllers[0].GetCurrentReading(buttonOneStates, null, axisOneStates);
-                    controllers[1].GetCurrentReading(buttonTwoStates, null, axisTwoStates);
+                    if (!(RawGameControllers.Count == 0))
+                    {
+                        controllers[0].GetCurrentReading(buttonOneStates, null, axisOneStates);
+                        controllers[1].GetCurrentReading(buttonTwoStates, null, axisTwoStates);
 
-                    for (int a = 0; a < buttonOneStates.Length; a++)
-                    {
-                        deploy += "Button " + a + ": " + buttonOneStates[a] + " |";
-                    }
-                    PilotOne.Text = deploy;
-                    if (buttonOneStates[0])
-                    {
-                        //cyka blyAT
-                    }
-                    else if (buttonOneStates[1])
-                    {
+                        for (int a = 0; a < buttonOneStates.Length; a++)
+                        {
+                            deploy += "Button " + a + ": " + buttonOneStates[a] + " |";
+                        }
+                        PilotOne.Text = deploy;
+                        if (buttonOneStates[0])
+                        {
+                            //cyka blyAT
+                        }
+                        else if (buttonOneStates[1])
+                        {
 
-                    }
-                    else if (buttonOneStates[2])
-                    {
+                        }
+                        else if (buttonOneStates[2])
+                        {
 
-                    }
-                    else if (buttonOneStates[3])
-                    {
+                        }
+                        else if (buttonOneStates[3])
+                        {
 
-                    }
-                    else if (buttonOneStates[4])
-                    {
+                        }
+                        else if (buttonOneStates[4])
+                        {
 
-                    }
-                    else if (buttonOneStates[5])
-                    {
+                        }
+                        else if (buttonOneStates[5])
+                        {
 
-                    }
-                    else if (buttonOneStates[6])
-                    {
+                        }
+                        else if (buttonOneStates[6])
+                        {
 
-                    }
-                    else if (buttonOneStates[7])
-                    {
+                        }
+                        else if (buttonOneStates[7])
+                        {
 
-                    }
-                    else if (buttonOneStates[8])
-                    {
+                        }
+                        else if (buttonOneStates[8])
+                        {
 
-                    }
-                    else if (buttonOneStates[9])
-                    {
+                        }
+                        else if (buttonOneStates[9])
+                        {
 
-                    }
-                    else if (buttonOneStates[10])
-                    {
+                        }
+                        else if (buttonOneStates[10])
+                        {
 
-                    }
-                    else if (buttonOneStates[11])
-                    {
+                        }
+                        else if (buttonOneStates[11])
+                        {
 
-                    }
-                    if (buttonTwoStates[0])
-                    {
+                        }
+                        if (buttonTwoStates[0])
+                        {
 
-                    }
-                    else if (buttonTwoStates[1])
-                    {
+                        }
+                        else if (buttonTwoStates[1])
+                        {
 
-                    }
-                    else if (buttonTwoStates[2])
-                    {
+                        }
+                        else if (buttonTwoStates[2])
+                        {
 
-                    }
-                    else if (buttonTwoStates[3])
-                    {
+                        }
+                        else if (buttonTwoStates[3])
+                        {
 
-                    }
-                    else if (buttonTwoStates[4])
-                    {
+                        }
+                        else if (buttonTwoStates[4])
+                        {
 
-                    }
-                    else if (buttonTwoStates[5])
-                    {
+                        }
+                        else if (buttonTwoStates[5])
+                        {
 
-                    }
-                    else if (buttonTwoStates[6])
-                    {
+                        }
+                        else if (buttonTwoStates[6])
+                        {
 
-                    }
-                    else if (buttonTwoStates[7])
-                    {
+                        }
+                        else if (buttonTwoStates[7])
+                        {
 
-                    }
-                    else if (buttonTwoStates[8])
-                    {
+                        }
+                        else if (buttonTwoStates[8])
+                        {
 
-                    }
-                    else if (buttonTwoStates[9])
-                    {
+                        }
+                        else if (buttonTwoStates[9])
+                        {
 
-                    }
-                    else if (buttonTwoStates[10])
-                    {
+                        }
+                        else if (buttonTwoStates[10])
+                        {
 
-                    }
-                    else if (buttonTwoStates[11])
-                    {
+                        }
+                        else if (buttonTwoStates[11])
+                        {
 
+                        }
+                        //a whole shit ton of if statements of what happens when buttons are pressed, put results into modbusRegisters and manipRegisters
+                        //See the arduino code for explanations of what each place in modbusRegisters represents. 
+                        SendBytes(modbusRegisters, manipRegisters);
                     }
-                    //a whole shit ton of if statements of what happens when buttons are pressed, put results into modbusRegisters and manipRegisters
-                    //See the arduino code for explanations of what each place in modbusRegisters represents. 
-                    SendBytes(modbusRegisters, manipRegisters);
                 }
-            }
-            catch (Exception ex)
-            {
-                connected = false;
-                ContentDialog failedtoConnectControllersDialog = new ContentDialog
+                catch (Exception ex)
                 {
-                    Title = "Boi where are the controllers?",
-                    Content = ex.ToString(),
-                    CloseButtonText = "OH SUGAR"
-                };
+                    connected = false;
+                    ContentDialog failedtoConnectControllersDialog = new ContentDialog
+                    {
+                        Title = "Controllers failed to connect",
+                        Content = ex.ToString(),
+                        CloseButtonText = "Ok"
+                    };
 
-                ContentDialogResult result = await failedtoConnectControllersDialog.ShowAsync();
-            }
+                    //ContentDialogResult result = await failedtoConnectControllersDialog.ShowAsync();
+                }
+            });
         }
 
         
